04b9ad6574a1fe54690ca063b9bf78208480b8a2
lolczak <lukasz.jan.olczak@gmail.com>
2017-04-18 20:43:35 +0200

    Refactoring:   - descriptive name

--- src/test/scala/tech/olczak/chesschallenge/chess/PieceSpec.scala
+++ src/test/scala/tech/olczak/chesschallenge/chess/PieceSpec.scala
@@ -8,24 +8,24 @@ import org.scalatest.{FlatSpec, Matchers}
 class PieceSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
 
   "A king" should "threaten one square in any direction" in new TestContext {
-    forAll(squareGen) { current =>
+    forAll(squareGen) { occupied =>
       //given
-      val threatenedSquares = for (x <- -1 to 1; y <- -1 to 1) yield Square(current.rank + x, current.file + y)
+      val threatenedSquares = for (x <- -1 to 1; y <- -1 to 1) yield Square(occupied.rank + x, occupied.file + y)
       val safeSquares = SampleBoard.allSquares.diff(threatenedSquares)
       //then
-      forEvery(threatenedSquares) { square => assert(King.isThreatened(current)(square)) }
-      forEvery(safeSquares) { square => assert(!King.isThreatened(current)(square)) }
+      forEvery(threatenedSquares) { square => assert(King.isThreatened(occupied)(square)) }
+      forEvery(safeSquares) { square => assert(!King.isThreatened(occupied)(square)) }
     }
   }
 
   "A rook" should "threaten any number of squares along any rank or file" in new TestContext {
-    forAll(squareGen) { current =>
+    forAll(squareGen) { occupied =>
       //given
-      val threatenedSquares = SampleBoard.allSquares filter { case Square(rank, file) => current.rank == rank || current.file == file }
+      val threatenedSquares = SampleBoard.allSquares filter { case Square(rank, file) => occupied.rank == rank || occupied.file == file }
       val safeSquares = SampleBoard.allSquares.diff(threatenedSquares)
       //then
-      forEvery(threatenedSquares) { square => assert(Rook.isThreatened(current)(square)) }
-      forEvery(safeSquares) { square => assert(!Rook.isThreatened(current)(square)) }
+      forEvery(threatenedSquares) { square => assert(Rook.isThreatened(occupied)(square)) }
+      forEvery(safeSquares) { square => assert(!Rook.isThreatened(occupied)(square)) }
     }
   }
 
