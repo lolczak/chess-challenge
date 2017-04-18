2d423e5a9a0097fc2f531d9a15a2cba23d1ed7f8
lolczak <lukasz.jan.olczak@gmail.com>
2017-04-18 19:29:54 +0200

    Refactoring:   - generator introduced   - inspector introduced   - descriptive names

--- src/test/scala/tech/olczak/chesschallenge/chess/PieceSpec.scala
+++ src/test/scala/tech/olczak/chesschallenge/chess/PieceSpec.scala
@@ -1,17 +1,33 @@
 package tech.olczak.chesschallenge.chess
 
+import org.scalacheck.Gen
+import org.scalatest.Inspectors._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
-class PieceSpec extends FlatSpec with Matchers {
+class PieceSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
 
-  "A king" should "threaten one square in any direction" in {
-    //given
-    val current = Square(3, 4)
-    val threatened = for (x <- -1 to 1; y <- -1 to 1) yield Square(current.rank + x, current.file + y)
-    val other = Board(5, 5).allSquares.diff(threatened)
-    //then
-    threatened.forall(King.isThreatened(current)) shouldBe true
-    other.forall(!King.isThreatened(current)(_)) shouldBe true
+  "A king" should "threaten one square in any direction" in new TestContext {
+    forAll(squareGen) { current =>
+      //given
+      val threatenedSquares = for (x <- -1 to 1; y <- -1 to 1) yield Square(current.rank + x, current.file + y)
+      val safeSquares = SampleBoard.allSquares.diff(threatenedSquares)
+      //then
+      forEvery(threatenedSquares) { square => assert(King.isThreatened(current)(square)) }
+      forEvery(safeSquares) { square => assert(!King.isThreatened(current)(square)) }
+    }
+  }
+
+  trait TestContext {
+
+    val SampleBoard = Board(10, 10)
+
+    val squareGen =
+      for {
+        rank <- Gen.choose(1, 10)
+        file <- Gen.choose(1, 10)
+      } yield Square(rank, file)
+
   }
 
 }
