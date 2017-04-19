2b713e34db63f69352f17c9fc1519b3a68ee5b23
lolczak <lukasz.jan.olczak@gmail.com>
2017-04-19 22:40:59 +0200

    BacktrackingSolver: brute force algorithm

--- src/main/scala/tech/olczak/chesschallenge/solver/BacktrackingSolver.scala
+++ src/main/scala/tech/olczak/chesschallenge/solver/BacktrackingSolver.scala
@@ -4,12 +4,19 @@ import tech.olczak.chesschallenge.chess._
 
 object BacktrackingSolver extends ChessChallengeSolver {
 
-  override def solve(board: Board, pieces: List[(Piece, Int)]): List[Arrangement] = {
-    if (pieces.isEmpty) List.empty
+  override def solve(board: Board, pieceCounts: List[(Piece, Int)]): List[Arrangement] = {
+    if (pieceCounts.isEmpty) List.empty
     else {
-      val (piece, count) = pieces.head
+      val pieces = pieceCounts flatMap { case (piece, count) => for (_ <- 1 to count) yield piece }
 
-      board.allSquares.map(square => Arrangement(board, List(PiecePosition(piece, square))))
+      board.allSquares.combinations(pieces.size).toList.par
+        .flatMap(_.permutations.toList)
+        .map(_.zip(pieces))
+        .map(_.map { case (s, p) => PiecePosition(p, s)})
+        .filter(positions => positions.combinations(2).forall { case List(p1, p2) => !p1.piece.isThreatened(p1.square)(p2.square) &&  !p2.piece.isThreatened(p2.square)(p1.square)} )
+        .map(p => Arrangement(board, p.toSet))
+        .toSet
+        .toList
     }
   }
 
--- src/test/scala/tech/olczak/chesschallenge/solver/BacktrackingSolverSpec.scala
+++ src/test/scala/tech/olczak/chesschallenge/solver/BacktrackingSolverSpec.scala
@@ -5,6 +5,8 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 import tech.olczak.chesschallenge.chess._
 
+import scala.language.implicitConversions
+
 class BacktrackingSolverSpec extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
 
   "A backtracking solver" should "not find a solution when there is no pieces" in new TestContext {
@@ -19,7 +21,16 @@ class BacktrackingSolverSpec extends FlatSpec with Matchers with GeneratorDriven
       //when
       val solutions = objectUnderTest.solve(board, List(Queen -> 1))
       //then
-      solutions should contain theSameElementsAs genAllSquares(board).map(s => Arrangement(board, List(PiecePosition(Queen, s))))
+      solutions should contain theSameElementsAs genAllSquares(board).map(s => Arrangement(board, Set(PiecePosition(Queen, s))))
+    }
+  }
+
+  it should "solve real world problem" in new TestContext {
+    forAll(RealWorldProblems) { case (board, pieces, expectedSolutions) =>
+      //when
+      val solutions = objectUnderTest.solve(board, pieces)
+      //then
+      solutions should contain only (expectedSolutions: _*)
     }
   }
 
@@ -29,12 +40,23 @@ class BacktrackingSolverSpec extends FlatSpec with Matchers with GeneratorDriven
 
     val boardGen =
       for {
-        rankCount <- Gen.choose(1, 10)
-        fileCount <- Gen.choose(1, 10)
+        rankCount <- Gen.choose(1, 2)
+        fileCount <- Gen.choose(1, 2)
       } yield Board(rankCount, fileCount)
 
     def genAllSquares(dimension: Board) =
       for (rank <- 0 until dimension.rankCount; file <- 0 until dimension.fileCount) yield Square(rank, file)
+
+    implicit def toPiecePosition(tuple: (Piece, (Int, Int))): PiecePosition = PiecePosition(tuple._1, Square(tuple._2._1, tuple._2._2))
+
+    val RealWorldProblems = Gen oneOf Seq(
+      (Board(3, 3), List(King -> 2, Rook -> 1), List(
+        Arrangement(Board(3, 3), Set(King ->(0, 0), King ->(0, 2), Rook ->(2, 1))),
+        Arrangement(Board(3, 3), Set(King ->(0, 0), Rook ->(1, 2), King ->(2, 0))),
+        Arrangement(Board(3, 3), Set(King ->(0, 2), Rook ->(1, 0), King ->(2, 2))),
+        Arrangement(Board(3, 3), Set(Rook ->(0, 1), King ->(2, 0), King ->(2, 2)))
+      ))
+    )
 
   }
 
