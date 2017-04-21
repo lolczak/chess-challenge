c21c7e5e117917ee585485cd8a431dbfdf40a43f
lolczak <lukasz.jan.olczak@gmail.com>
2017-04-21 17:17:15 +0200

    Refactoring:   - method extracted

--- src/main/scala/tech/olczak/chesschallenge/app/ChessChallengeApp.scala
+++ src/main/scala/tech/olczak/chesschallenge/app/ChessChallengeApp.scala
@@ -6,6 +6,7 @@ import tech.olczak.chesschallenge.app.cli.ParseFailure
 import tech.olczak.chesschallenge.app.effect.ConsoleIO._
 import tech.olczak.chesschallenge.app.effect.SystemIO._
 import tech.olczak.chesschallenge.app.effect.{ConsoleIO, SystemIO}
+import tech.olczak.chesschallenge.solver.ChessConfig
 
 import scala.concurrent.duration.Duration
 import scalaz._
@@ -18,21 +19,20 @@ object ChessChallengeApp {
   val mainCmd = ReaderT[Free[IO, ?], Environment, Unit] { env: Environment =>
     for {
       _ <- printLine[IO]("Hello, starting chess challenge app...")
-      _ <- env.cliParser.parse(env.args) match {
-        case -\/(failure) =>
-          handleParseFailure(failure)
-        case \/-(chessConfig) =>
-          for {
-            _         <- printLine[IO](s"Looking solutions for ${chessConfig.board} and ${chessConfig.pieceGroups}")
-            start     <- getCurrentMillis[IO]
-            solutions =  env.solver.solve(chessConfig)
-            end       <- getCurrentMillis[IO]
-            duration   = Duration(end - start, TimeUnit.MILLISECONDS)
-            _         <- printLine[IO](s"Found ${solutions.size} solutions.")
-            _         <- printLine[IO](s"Elapsed time: ${duration.toSeconds} sec and ${duration.toMillis % 1000} millis.")
-            _         <- solutions.traverseU(c => printLine[IO](c.shows))
-          } yield ()
-       }
+      _ <- env.cliParser.parse(env.args) fold (handleParseFailure, solve(_).run(env))
+    } yield ()
+  }
+
+  private def solve(chessConfig: ChessConfig) = ReaderT[Free[IO, ?], Environment, Unit] { env: Environment =>
+    for {
+      _         <- printLine[IO](s"Solving chess challenge for ${chessConfig.board} and ${chessConfig.pieceGroups}")
+      start     <- getCurrentMillis[IO]
+      solutions =  env.solver.solve(chessConfig)
+      end       <- getCurrentMillis[IO]
+      duration   = Duration(end - start, TimeUnit.MILLISECONDS)
+      _         <- printLine[IO](s"Found ${solutions.size} solutions.")
+      _         <- printLine[IO](s"Elapsed time: ${duration.toSeconds} sec and ${duration.toMillis % 1000} millis.")
+      _         <- solutions.traverseU(c => printLine[IO](c.shows))
     } yield ()
   }
 
@@ -42,6 +42,5 @@ object ChessChallengeApp {
       _ <- printError[IO]("Usage: sbt \"run [ranks] [files] [<piece symbol><piece count>...]\"")
       _ <- exit[IO](1)
     } yield ()
-
 
 }
