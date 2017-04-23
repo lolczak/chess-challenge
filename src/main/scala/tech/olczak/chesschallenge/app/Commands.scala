7db88b2132903ca39cfb6a997832446d39885695
lolczak <lukasz.jan.olczak@gmail.com>
2017-04-23 19:15:26 +0200

    Refactoring: messages extracted

--- src/main/scala/tech/olczak/chesschallenge/app/Commands.scala
+++ src/main/scala/tech/olczak/chesschallenge/app/Commands.scala
@@ -3,12 +3,13 @@ package tech.olczak.chesschallenge.app
 import java.util.concurrent.TimeUnit
 
 import tech.olczak.chesschallenge.app.cli.ParseFailure
+import tech.olczak.chesschallenge.app.constant.Constants._
+import tech.olczak.chesschallenge.app.constant.Messages._
 import tech.olczak.chesschallenge.app.effect.ConsoleIO._
 import tech.olczak.chesschallenge.app.effect.SystemIO._
 import tech.olczak.chesschallenge.app.effect.{ConsoleIO, SystemIO}
 import tech.olczak.chesschallenge.chess.Chessboard
 import tech.olczak.chesschallenge.solver.ChessConfig
-import tech.olczak.chesschallenge.app.constant.Constants._
 
 import scala.concurrent.duration.Duration
 import scalaz.Scalaz._
@@ -22,17 +23,17 @@ object Commands {
 
   val solverCmd = ReaderT[Free[Effect, ?], Environment, Unit] { env: Environment =>
     for {
-      _ <- printLine[Effect]("Hello, starting chess challenge app...")
+      _ <- printLine[Effect](GreetingMsg)
       _ <- env.cliParser.parse(env.args) fold (handleParseFailure, solve(_).run(env))
     } yield ()
   }
 
   private def solve(chessConfig: ChessConfig) = ReaderT[Free[Effect, ?], Environment, Unit] { env: Environment =>
     for {
-      _                     <- printLine[Effect](s"Solving chess challenge for ${chessConfig.board} and ${chessConfig.pieceGroups}")
+      _                     <- printLine[Effect](SolverStartMsg format (chessConfig.board, chessConfig.pieceGroups))
       (solutions, duration) <- measure { env.solver.solve(chessConfig) }
-      _                     <- printLine[Effect](s"Found ${solutions.size} solutions.")
-      _                     <- printLine[Effect](s"Elapsed time: ${duration.toSeconds} sec and ${duration.toMillis % MillisInSecond} millis.")
+      _                     <- printLine[Effect](FoundSolutionsMsg format solutions.size)
+      _                     <- printLine[Effect](ElapsedTimeMsg format (duration.toSeconds, duration.toMillis % MillisInSecond))
       _                     <- solutions traverseU printLine[Effect, Chessboard]
     } yield ()
   }
@@ -47,8 +48,8 @@ object Commands {
 
   private def handleParseFailure(failure: ParseFailure) =
     for {
-      _ <- printError[Effect](s"Invalid arguments: ${failure.msg}")
-      _ <- printError[Effect]("Usage: sbt \"run [ranks] [files] [<piece symbol><piece count>...]\"")
+      _ <- printError[Effect](InvalidArgumentsMsg format failure.msg)
+      _ <- printError[Effect](UsageMsg)
       _ <- exit[Effect](FailureExitCode)
     } yield ()
 
---
+++ src/main/scala/tech/olczak/chesschallenge/app/constant/Messages.scala
@@ -0,0 +1,17 @@
+package tech.olczak.chesschallenge.app.constant
+
+object Messages {
+
+  val GreetingMsg = "Hello, starting chess challenge app..."
+
+  val InvalidArgumentsMsg = "Invalid arguments: %s"
+
+  val UsageMsg = "Usage: sbt \"run [ranks] [files] [<piece symbol><piece count>...]\""
+
+  val SolverStartMsg = "Solving chess challenge for %s and %s"
+
+  val FoundSolutionsMsg = "Found %d solutions."
+
+  val ElapsedTimeMsg = "Elapsed time: %d sec and %d millis."
+
+}
